0ee60d8d49bcba567946cdd6bed293893ea6e60b
leejh0315 <yijh0315@naver.com>
2023-12-11 13:48:08 +0900

    first commit

---
+++ src/test/java/project/DIY/DiyApplicationTests.java
@@ -0,0 +1,13 @@
+package project.DIY;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
