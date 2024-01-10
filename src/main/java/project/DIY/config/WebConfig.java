f36da6db0e70581acf3eacb6e826324c4f5118f9
leejh0315 <yijh0315@naver.com>
2024-01-10 18:32:26 +0900

    fix : joinValidator

--- src/main/java/project/DIY/config/WebConfig.java
+++ src/main/java/project/DIY/config/WebConfig.java
@@ -1,24 +1,37 @@
 package project.DIY.config;
 
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer{
 	
-/*
-    @Value("${resource.handler}")
-    private String resourceHandler;
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
 
-    @Value("${resource.location}")
-    private String resourceLocation;
-*/
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        // 템플릿 리졸버 설정
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("classpath:/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        return templateResolver;
+    }
+	
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		// TODO Auto-generated method stub
--- src/main/java/project/DIY/domain/Member.java
+++ src/main/java/project/DIY/domain/Member.java
@@ -2,14 +2,29 @@ package project.DIY.domain;
 
 import java.util.Date;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class Member {
+
 	private int id;
+	
+	@Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
+	@NotBlank(message = "이메일은 필수 입력 값입니다.")
 	private String loginId;
+	
+	
+	@NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+	@Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
 	private String password;
+	
+	@Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
 	private String nickName;
+	
 	private String statusCode;
 	private String birth;
 	private String activeUUID;
